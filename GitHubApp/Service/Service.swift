ff8bbb8bff663a6edd5cd188218cd5f8a6e550e5
Taciano <maximotco@hotmail.com>
2023-04-15 14:35:31 -0300

    Update Service.swift

--- GitHubApp/Service/Service.swift
+++ GitHubApp/Service/Service.swift
@@ -11,5 +11,26 @@ struct Service {
     func fetchList(of user: String, completion: @escaping ([Repository]?) -> Void) {
         
         // TODO
+        guard let url = URL(string: "https://api.github.com/users/\(user)/repos") else { return }
+        
+        network.performGet(url: url) { data in
+            completion(parseJson(from: data))
+        }
+        
+    }
+    
+    func parseJson(from data: Data?) -> [Repository]? {
+        
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedJson = try decoder.decode([Repository].self, from: data ?? Data())
+            return decodedJson
+        } catch {
+            return nil
+        }
+        
     }
 }
+
+
